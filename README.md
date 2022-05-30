80151b96c05da33e62a5a1877c94055acac99801
heyrict <xiezh0831@yahoo.co.jp>
2022-05-30 08:31:41 +0800

    feat: Add wayland support

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Kaomoji Rofi
 
+This is a fork of https://gitlab.com/ceda_ei/kaomoji-rofi with wayland modifications.
+
+---
+
 Rofi script for typing kaomojis. Types kaomojis and copies them into your clipboard.
 ![Kaomoji Rofi Preview](ss.png)
 
 ## Requirements
 
+For X11:
+
 + `rofi`
 + `xdotool`
 + `xclip`
+
+For Wayland:
+
++ `wl-copy`
++ `wtype`
 
 ## Installation
 
--- kaomoji.sh
+++ kaomoji.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-current_wid=$(xdo id)
-selection=$(rofi -i -dmenu $@ < $(dirname $0)/kaomoji.txt)
-kaomoji=$(echo $selection | sed "s|$(echo -e "\ufeff").*||")
-echo -n "$kaomoji" | xclip -selection clipboard
-xdotool key --window $current_wid --clearmodifiers ctrl+v
+if [ -z $WAYLAND_DISPLAY ]; then
+    current_wid=$(xdo id)
+    selection=$(rofi -i -dmenu $@ < $(dirname $0)/kaomoji.txt)
+    kaomoji=$(echo $selection | sed "s|$(echo -e "\ufeff").*||")
+    echo -n "$kaomoji" | xclip -selection clipboard
+    xdotool key --window $current_wid --clearmodifiers ctrl+v
+else
+    kaomoji=$(rofi -i -dmenu -p kaomoji $@ < $(dirname $0)/kaomoji.txt | sed "s|$(echo -e "\ufeff").*||")
+    wtype "$kaomoji"
+fi
